283513550a3c733b1623186c04af18f98891c6ea
yoo sung jun (유성준) <127585771+dbstarjun@users.noreply.github.com>
2023-03-16 13:19:09 +0900

    세로 나형
    수정본

--- work03_16_1/js/script.js
+++ work03_16_1/js/script.js
@@ -1,8 +1,15 @@
 $(".menu ul li").hover(
     function() {
-        $(this).children("ul").slideDown("slow");
+        $(this).children("ul").stop().slideDown("slow");
     },
     function() {
-        $(this).children("ul").slideUp("fast");
+        $(this).children("ul").stop().slideUp("fast");
+    }
+);
+
+$(".menu ul li").click(
+    function() {
+        $(".menu ul li ul").hide();
+        $(".this").children("ul").slideDown("slow");
     }
 );
