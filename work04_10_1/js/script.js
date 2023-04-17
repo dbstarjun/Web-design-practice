2c71bbed124dbd2ccbded9ec71634158a4c28d26
yoo sung jun (유성준) <127585771+dbstarjun@users.noreply.github.com>
2023-04-17 11:14:25 +0900

    .

--- work04_10_1/js/script.js
+++ work04_10_1/js/script.js
@@ -1,40 +1,40 @@
-$(function () {
+$(function(){
 
-    //header menu------------------------------------
-    $('li.lv1').mouseenter(function () {
-        $(this).find('ul.lv2').stop().slideDown()
+    $('li.mainmenu').mouseenter(function(){
+        $(this).find('ul.submenu').stop().slideDown()
     })
-    $('li.lv1').mouseleave(function () {
-        $('ul.lv2').stop().slideUp()
+    $('li.mainmenu').mouseleave(function(){
+        $('ul.submenu').stop().slideUp()
     })
-    var slide = $(".slide>img");
-    var sno = 0;
-    var eno = slide.length - 1;
-    var timer = setInterval("autosilde()", 3000);
 
-    function autosilde() {
-        $(slide[sno]).stop().animate({
-            opacity: 0
-        }, 1000, function () {
-            // $(this).css({  opacity: "100%" });
-        });
-        sno++;
-        if (sno > eno) {
-            sno = 0;
-        }
-        $(slide[sno]).stop().animate({
-            opacity: 1
-        }, 1000)
+var slide = $(".slide>img");
+var sno = 0;
+var eno = slide.length - 1;
+var timer = setInterval("autosilde()", 3000);
+
+function autosilde() {
+    $(slide[sno]).stop().animate({
+        opacity: 0
+    }, 1000, function () {
+        // $(this).css({  opacity: "100%" });
+    });
+    sno++;
+    if (sno > eno) {
+        sno = 0;
     }
+    $(slide[sno]).stop().animate({
+        opacity: 1
+    }, 1000)
+}
 
 
 
-    //popUpBox------------------------------------
-    $('.notice li:nth-child(1)').click(function () {
-        $('.popUpBox').show()
-    })
-    $('.popUpBox button').click(function () {
-        $('.popUpBox').hide()
-    })
+//popUpBox------------------------------------
+$('.notice li:nth-child(1)').click(function () {
+    $('.popUpBox').show()
+})
+$('.popUpBox button').click(function () {
+    $('.popUpBox').hide()
+})
 
 })
