660cf7f9fb68614714a1094f789649c5a9ee641c
yoo sung jun (유성준) <127585771+dbstarjun@users.noreply.github.com>
2023-04-28 10:31:23 +0900

    .

--- work04_24/js/script.js
+++ work04_24/js/script.js
@@ -1,18 +1,37 @@
-// 이미지만 이동
-var slide = $(".slide > div");
-var sno = 0;
-var eno = slide.length -1;
-var timer = setInterval("autoslide()", 3000);
+$(function(){
+    $('li.lv1').mouseenter(function(){
+        $('ul.lv2, .headerBg').stop().slideDown()
+    })
+    $('li.lv1').mouseleave(function(){
+        $('ul.lv2, .headerBg').stop().slideUp()
+    })
+    
 
-function autoslide() {
-    $(slide[sno]).animate({
-        left:"-100%"
-    },1000, function(){
-        $(this).css({left:"100%"});
-    });
-    sno++;
-    if(eno < sno) sno =0;
-    $(slide[sno]).animate({
-        left:"0"
-    },1000);
-}
+    setInterval(function(){
+        $('.slideWrap').animate({'marginLeft':'-100%'}, function(){
+            $('.slide:first').appendTo('.slideWrap')
+            $('.slideWrap').css({'marginLeft':'0'})
+        })
+    },3000)
+    
+
+    $('.tabMenu li').click(function(){
+        $('.tabMenu li').removeClass('on')
+        $(this).addClass('on')
+    
+        var idx=$(this).index()
+        $('.tabItem >*').hide().removeClass('on')
+        $('.tabItem >*').eq(idx).show().addClass('on')
+    })
+    
+
+    $('.notice li:nth-child(1)').click(function(){
+        $('.popUpBox').show();
+    })
+    
+    $('.popUpBox button').click(function(){
+        $('.popUpBox').hide();
+    })
+    
+    
+    })
