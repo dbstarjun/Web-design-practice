1543b2a4f6085d059bd15e0aa06a9a22601a34df
yoo sung jun (유성준) <127585771+dbstarjun@users.noreply.github.com>
2023-06-09 18:50:56 +0900

    .

--- work06_09/js/script.js
+++ work06_09/js/script.js
@@ -3,6 +3,12 @@ var sno = 0;
 var eno = slide.length-1;
 var timer = setInterval("autoslide()", 3000);
 
-function autoslide(
+function autoslide() {
+    $(slide[sno]).animate({
+        left: "100%"
+    },1000, function() {
+        $(this).css({left:"-100%"});
+    });
+    sno ++;
     
-) 
+} 
